a292fad1be9dd9decde2d9c0b8489f183d7759df
Adam Harbisher <aharbisher@gmail.com>
2022-02-25 18:01:04 +0000

    Created User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_22_174543) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_25_175632) do
   create_table "topics", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
