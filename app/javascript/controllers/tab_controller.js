e78d8dc49eb5c129a08907a3b198a0312e99aa21
Adam Harbisher <aharbisher@gmail.com>
2022-03-05 18:20:49 +0000

    Refactored code to use dynamic links with navbar yield

--- app/javascript/controllers/tab_controller.js
+++ app/javascript/controllers/tab_controller.js
@@ -1,20 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import { Tab } from "bootstrap"
 
 // Connects to data-controller="tab"
 export default class extends Controller {
   connect() {
-  }
-
-  show() {
-    var triggerTabList = [].slice.call(document.querySelectorAll('#pills-tab button'))
-      triggerTabList.forEach(function (triggerEl) {
-      var tabTrigger = new Tab(triggerEl)
-    
-      triggerEl.addEventListener('click', function (event) {
-        event.preventDefault()
-        tabTrigger.show()
-      })
-    })    
   }
 }
